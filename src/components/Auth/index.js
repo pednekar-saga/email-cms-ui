54f0e20d40c8ff75fe0a097b500801b884709272
Sagar Pednekar <sagarpednekar04@gmail.com>
2019-10-26 15:47:19 +0530

    Fix(Login: Remove unwanted imports and stylesheets )

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,44 +2,48 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import Login from "../Login";
 import { getItem, setItem, removeItem } from "../../shared/store";
-const Auth = ({ props }) => {
-   const [loggedIn, setLoggedIn] = useState({ loggedIn: false });
-   useEffect(() => {
-      var query = queryString.parse(props.location.search);
 
-      if (query.token) {
-         setItem("token", query.token);
-         setLoggedIn({
-            loggedIn: true
-         });
-         props.history.push("/");
-      }
-      return;
-   }, []);
-   const logout = item => {
+
+const Auth = ({ location, history }) => {
+  const [loggedIn, setLoggedIn] = useState({ loggedIn: false });
+  useEffect(() => {
+    var query = queryString.parse(location.search);
+
+    if (query.token) {
+      setItem("token", query.token);
       setLoggedIn({
-         loggedIn: false
+        loggedIn: true
       });
-      removeItem(item);
-      props.history.push("/");
-   };
-   return (
-      <div>
-         {loggedIn && getItem("token") ? (
-            <div>
-               <div>Logout Component </div>
-               <button
-                  onClick={() => {
-                     logout("token");
-                  }}
-               >
-                  Log out
-               </button>
-            </div>
-         ) : (
-            <Login />
-         )}
-      </div>
-   );
+      history.push("/");
+    }
+
+    return query;
+  }, []);
+
+  const logout = item => {
+    setLoggedIn({
+      loggedIn: false
+    });
+    removeItem(item);
+    history.push("/");
+  };
+  return (
+    <div>
+      {loggedIn && getItem("token") ? (
+        <div>
+          <div>Logout Component </div>
+          <button
+            onClick={() => {
+              logout("token");
+            }}
+          >
+            Log out
+          </button>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 };
 export default Auth;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Auth from '../src/components/Auth'
 
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+
+/**
+ * @description: Wraps auth component 
+ * @param {any} props 
+ * @returns 
+ */
+
+
+function Home(props) {
+  return (
+    <div>
+      <Auth props={props} />
+    </div>
+  );
+}
+
+
+/** 
+ * @description: Render main component 
+ */
+
 
 ReactDOM.render(
-   <Router>
-      <Switch>
-         <Route path="/" component={App} />
-      </Switch>
-   </Router>,
-   document.getElementById("root")
+  <Router>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
